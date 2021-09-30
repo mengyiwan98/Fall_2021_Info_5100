4097c342eca3e3ca4d3231fed9d7fbb99a5fc16f
mengyiwan98 <90485487+mengyiwan98@users.noreply.github.com>
2021-09-30 15:51:04 -0700

    Question4

---
+++ HW1/src/edu/northeastern/mengyi/Question4.java
@@ -0,0 +1,19 @@
+package edu.northeastern.mengyi;
+
+public class Question4 {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] array = new int[26];
+        int[] array2 = new int[26];
+        for (int i = 0; i < ransomNote.length(); i++) {
+            array[ransomNote.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < magazine.length(); i++) {
+            array2[magazine.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (array[i] != 0 && array[i] > array2[i]) return false;
+        }
+        return true;
+    }
+}
+
---
+++ HW1/src/edu/northeastern/mengyi/Question5.java
@@ -0,0 +1,4 @@
+package edu.northeastern.mengyi;
+
+public class Question5 {
+}
