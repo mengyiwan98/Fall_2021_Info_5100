89f8d2c7588bda10a15815e9ab1f07196ab09ca2
mengyiwan98 <90485487+mengyiwan98@users.noreply.github.com>
2021-09-30 16:22:53 -0700

    Mengyi Wan

---
+++ Assignment/src/edu/northeastern/mengyi/Q1.java
@@ -0,0 +1,21 @@
+package edu.northeastern.mengyi;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    public int sumOfUnique(int[] nums) {
+        int[] arr = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            arr[nums[i]]++;
+        }
+
+        int sum = 0;
+        for (int j = 1; j <= nums.length - 1; j++) {
+            if (arr[j] == 1) sum += j;
+        }
+
+        return sum;
+    }
+}
